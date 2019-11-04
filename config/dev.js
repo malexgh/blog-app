5500f66ff1af8af290f8f85d3702024051e30a34
Marcio <alexandrino.marcio@gmail.com>
2019-11-04 11:54:54 -0300

    first commit

---
+++ config/dev.js
@@ -0,0 +1,8 @@
+module.exports = {
+  googleClientID:
+    '964808011168-29vqsooppd769hk90kjbjm5gld0glssb.apps.googleusercontent.com',
+  googleClientSecret: 'KnH-rZC23z4fr2CN4ISK4srN',
+  mongoURI: 'mongodb+srv://blog:blog@cluster0-ze3sn.mongodb.net/blog?retryWrites=true&w=majority',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
+};
