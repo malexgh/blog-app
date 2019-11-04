32ccf92ce6af3766ab11a8b10bc1787ee54ea8bc
Marcio <alexandrino.marcio@gmail.com>
2019-11-04 16:55:11 -0300

    test fix

--- config/keys.js
+++ config/keys.js
@@ -1,6 +1,6 @@
 if (process.env.NODE_ENV === 'production') {
   module.exports = require('./prod');
-} else if (process.env.NODE_ENV === 'ci') {
+} else if (process.env.NODE_ENV === 'TEST') {
   module.exports = require('./ci');
 } else {
   module.exports = require('./dev');
